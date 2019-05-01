39178a1c2df81d75da76f126bab821490e48e84a
Mario Amaya <thefozcat@gmail.com>
2019-05-01 15:24:30 -0700

    added GET by id to cohorts-router

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -18,6 +18,8 @@ const knexConfig = {
 
 const db = knex(knexConfig);
 
+// GET cohorts
+
 router.get('/', (req, res) => {
     db('cohorts')
         .then(cohort => {
@@ -25,6 +27,24 @@ router.get('/', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({ errorMessage: 'Could not retrieve cohort data.' })
+        })
+})
+
+// GET cohort by id
+
+router.get('/:id', (req, res) => {
+    db('cohorts')
+        .where({ id: req.params.id })
+        .first()
+        .then(cohort => {
+            if (cohort) {
+                res.status(201).json(cohort)
+            } else {
+                res.status(404).json({ errorMessage: 'A cohort with the specified ID could not be found.' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'Could not retrieve cohort data.' })
         })
 })
 
